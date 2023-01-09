823a2a4c1efd036be62c82d81c28562b35674474
Matthias Rolke <mr.amtrack@gmail.com>
2023-01-09 16:31:46 +0100

    refactor: replace keytar package with sfdx keychain implementation
    The npm package `keytar` depends on Python which we want to get rid of.
    `@salesforce/core` provides a keychain implementation for Windows/MacOS/Unix.
    
    [HUT-245]
    
    BREAKING CHANGE: For existing installations you might be prompted once
    to grant access to the hutte-io credential in your keychain of your OS.
    
    For example on MacOS, the process accessing the keychain was changed from "node" to "security".
    So you might be prompted:
    
    > security wants to use your confidential information stored in "hutte-io" in your keychain.
    >
    > To allow this, enter the "login" keychain password.
    
    Here you can enter your keychain password and click "Always Allow".

--- src/keychain.ts
+++ src/keychain.ts
@@ -1,14 +1,42 @@
-import { getPassword, setPassword } from 'keytar';
+import { retrieveKeychain } from '@salesforce/core/lib/keyChain';
+import { platform } from 'os';
 
 const SERVICE = 'hutte-io';
 
-function getUserApiToken(params: { userId: string }): Promise<string> {
-  return getPassword(SERVICE, params.userId);
+async function getUserApiToken(params: { userId: string }): Promise<string> {
+  const keychain = await retrieveKeychain(platform());
+  const password = await new Promise<string>((resolve, reject) => {
+    keychain.getPassword(
+      { service: SERVICE, account: params.userId },
+      (err, password) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(password);
+        }
+      },
+    );
+  });
+  return password;
 }
 
-function storeUserApiToken(params: { userId: string; apiToken: string }) {
-  setPassword(SERVICE, params.userId, params.apiToken).catch(e => {
-    process.exit();
+async function storeUserApiToken(params: { userId: string; apiToken: string }) {
+  const keychain = await retrieveKeychain(platform());
+  await new Promise<string>((resolve, reject) => {
+    keychain.setPassword(
+      {
+        service: SERVICE,
+        account: params.userId,
+        password: params.apiToken,
+      },
+      (err, password?) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(password);
+        }
+      },
+    );
   });
 }
 
